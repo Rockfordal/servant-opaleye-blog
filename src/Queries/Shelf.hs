357cbdef9b5800c162f34f4fa72066eb5452ca81
Anders Lundström <anders@urkraft.se>
2016-08-08 15:00:34 +0200

    onödiga imports, id som inte krockar, städ

--- src/Queries/Shelf.hs
+++ src/Queries/Shelf.hs
@@ -7,27 +7,25 @@ import Control.Arrow (returnA)
 
 import App
 import Models.Shelf
-import Models.Item
-import Models.Depot
 
 
 shelfsQuery :: Query ShelfColumnRead
 shelfsQuery = queryTable shelfTable
 
 shelfByIdQuery :: ShelfID -> Query ShelfColumnRead
-shelfByIdQuery id = proc () -> do
+shelfByIdQuery shelfid = proc () -> do
                         shelf     <- shelfsQuery -< ()
-                        restrict -< shId shelf .== pgInt8 id
-                        returnA  -< shelf
+                        restrict -<  shId shelf .== pgInt8 shelfid
+                        returnA  -<  shelf
 
 shelfsBySizeQuery :: ShelfSize -> Query ShelfColumnRead
 shelfsBySizeQuery size = proc () -> do
                                shelf     <- shelfsQuery -< ()
-                               restrict -< shSize shelf .== pgInt4 size
-                               returnA  -< shelf
+                               restrict -<  shSize shelf .== pgInt4 size
+                               returnA  -<  shelf
 
 shelfsByLabelQuery :: ShelfLabel -> Query ShelfColumnRead
 shelfsByLabelQuery name = proc () -> do
   shelf     <- shelfsQuery -< ()
-  restrict -< shLabel shelf .== pgString name
-  returnA  -< shelf
+  restrict -<  shLabel shelf .== pgString name
+  returnA  -<  shelf
